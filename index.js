7b8e294c59f1f1b1e4846804cd2e6fe8f4cc00ff
Mostafa Elgebaly <moustafamohamed866@gmail.com>
2026-01-05 14:06:32 +0200

    4 new files added index.html / index.css / index.js / CursedTimerUlil-Aznm.ttf

---
+++ index.js
@@ -0,0 +1,39 @@
+let digitBanner = document.getElementById("home-digit-banner");
+// console.log(digitBanner)
+let Button1 = 0
+
+function increment1() {
+    Button1 +=1
+    digitBanner.textContent = Button1;
+}
+
+function increment2() {
+    Button1 +=2
+    digitBanner.textContent = Button1;
+}
+
+function increment3() {
+    Button1 +=3
+    digitBanner.textContent = Button1;
+}
+
+
+
+let digitBannerg = document.getElementById("guest-digit-banner");
+// console.log(digitBannerg)
+let Button1g = 0
+
+function increment1g() {
+    Button1g +=1
+    digitBannerg.textContent = Button1g;
+}
+
+function increment2g() {
+    Button1g +=2
+    digitBannerg.textContent = Button1g;
+}
+
+function increment3g() {
+    Button1g +=3
+    digitBannerg.textContent = Button1g;
+}
